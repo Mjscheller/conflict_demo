5eea598a7310da60a39a7b2b548aafe8727440ed
Michael Scheller <michaeljscheller@gmail.com>
2024-03-26 20:08:55 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 22e1a367b47891216bf40f94806860a6912bdb07
+    hello_world('NYC', 'New York')
 
 
 main()
